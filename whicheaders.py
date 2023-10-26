18150011a9956ac109c3d0f3e687d89d3963819e
n3wadm1n <100295059+n3wadm1n@users.noreply.github.com>
2023-10-26 16:59:35 -0300

    Update whicheaders.py
    Fixed an unknown false-positive added headers to confirmation.

--- whicheaders.py
+++ whicheaders.py
@@ -1,26 +1,43 @@
 #!/usr/bin/python3
 ####################
-################ 2.0
+################ 3.0
 ### n3wadm1n #####
 ### Euribot  #####
 #####################
 
-import argparse
-import requests
-import urllib3
-import readline
+import argparse, requests, urllib3, readline, random
 from requests.exceptions import RequestException
 from colored import fg, attr
 
 readline.set_completer_delims(' \t\n=')
 readline.parse_and_bind("tab: complete")
 
+user_agents = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:94.0) Gecko/20100101 Firefox/94.0",
+    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chromium/94.0.4606.71 Chrome/94.0.4606.71 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_6_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 12.0; AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15 Edg/97.0.1072.55",
+    "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0)",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36 OPR/81.0.4196.73",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1.2 Safari/605.1.15",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299"
+]
+
+rnd_u_ag = random.choice(user_agents)
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def check_headers(url):
     try:
-        response = requests.get(url, timeout=5, verify=False)
-        headers = response.headers
+        hders = {'User-Agent': rnd_u_ag}
+        response = requests.get(url, headers=hders, timeout=5, verify=False)
         required_headers = [
             "X-XSS-Protection",
             "X-Frame-Options",
